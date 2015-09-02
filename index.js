b34a2e55916975a6ab267d911c586de0579fbfb5
Umran Hussein <umran.hussein@gmail.com>
2015-09-02 16:37:44 +0500

    moar bug fixes!!

--- index.js
+++ index.js
@@ -32,82 +32,83 @@ var server = tls.createServer(tlsOptions, function (socket) {
 			socket.end();
 		}
 	}
+	
+	// define it here so it can be unassigned
+	var handler = function(data) {
+		buffer += data.toString();
+
+		if (buffer.indexOf("\r\n\r\n") > -1 || buffer.indexOf("\n\n") > -1) {
+			socket.removeListener('data', handler);
+
+			var captures = buffer.match(/^CONNECT ([^:]+):([0-9]+) (HTTP\/1\.[01])/);
+
+			if (!captures || captures.length < 2) {
+				console.log('Received invalid HTTP request');
+				return send_response(400, 'Bad Request', true);
+			}
+
+			var tmp = captures[1].split('~');
+			var target = tmp[0];
+			var port = captures[2];
+			console.log('Client requested a tunnel to ' + target + ' port ' + port);
+
+			http_version = captures[3];
+
+			if (!port) { 
+				return send_response(401, 'Unknown Proxy Target', true);
+			}
+
+			trans(target, port, target + ':' + port, function(err, remote) {
+		
+				if(err){
+					console.log(err);
+					send_response(500, 'Remote node refused tunnel or does not respond', true);
+					return;
+				}
+		
+				send_response(200, 'Connection Established');
+				console.log('Connected to upstream service, initiating tunnel pumping');
+		
+				// define a kill switch for this tunnel
+		
+				var killSwitch = new events.EventEmitter();
+		
+				killSwitch.once('disconnect', function() {
+					console.log('An error was encountered, forcing tunnel to close'); 
+					socket.end(); 
+					remote.end();
+				});
+
+				var tunnel = function(other) {
+					return function(data) {
+						other.write(data); 
+					}
+				}
+
+				socket.addListener('data', tunnel(remote));
+				remote.addListener('data', tunnel(socket));
+
+				socket.addListener('close', function(){
+					console.log('client socket closed connection, all sockets will now close or have been closed');
+					remote.end();
+				});
+				remote.addListener('close', function(){
+					console.log('remote socket closed connection, all sockets will now close or have been closed');
+					socket.end();
+				});
+
+				socket.addListener('error', function(){
+					killSwitch.emit('disconnect');
+				});
+				remote.addListener('error', function(){
+					killSwitch.emit('disconnect');
+				});
+			});
+		}
+	}
+	
 	socket.addListener('data', handler);
 });
-
-// define it here so it can be unassigned
-var handler = function(data) {
-	buffer += data.toString();
-
-	if (buffer.indexOf("\r\n\r\n") > -1 || buffer.indexOf("\n\n") > -1) {
-		socket.removeListener('data', handler);
-
-		var captures = buffer.match(/^CONNECT ([^:]+):([0-9]+) (HTTP\/1\.[01])/);
-
-		if (!captures || captures.length < 2) {
-			console.log('Received invalid HTTP request');
-			return send_response(400, 'Bad Request', true);
-		}
-
-		var tmp = captures[1].split('~');
-		var target = tmp[0];
-		var port = captures[2];
-		console.log('Client requested a tunnel to ' + target + ' port ' + port);
-
-		http_version = captures[3];
-
-		if (!port) { 
-			return send_response(401, 'Unknown Proxy Target', true);
-		}
-
-		trans(target, port, target + ':' + port, function(err, remote) {
-		
-			if(err){
-				console.log(err);
-				send_response(500, 'Remote node refused tunnel or does not respond', true);
-				return;
-			}
-		
-			send_response(200, 'Connection Established');
-			console.log('Connected to upstream service, initiating tunnel pumping');
-		
-			// define a kill switch for this tunnel
-		
-			var killSwitch = new events.EventEmitter();
-		
-			killSwitch.once('disconnect', function() {
-				console.log('An error was encountered, forcing tunnel to close'); 
-				socket.end(); 
-				remote.end();
-			});
-
-			var tunnel = function(other) {
-				return function(data) {
-					other.write(data); 
-				}
-			}
-
-			socket.addListener('data', tunnel(remote));
-			remote.addListener('data', tunnel(socket));
-
-			socket.addListener('close', function(){
-				console.log('client socket closed connection, all sockets will now close or have been closed');
-				remote.end();
-			});
-			remote.addListener('close', function(){
-				console.log('remote socket closed connection, all sockets will now close or have been closed');
-				socket.end();
-			});
-
-			socket.addListener('error', function(){
-				killSwitch.emit('disconnect');
-			});
-			remote.addListener('error', function(){
-				killSwitch.emit('disconnect');
-			});
-		});
-	}
-}
 
 // initialize tunnelling proxy
 server.listen(3080);
